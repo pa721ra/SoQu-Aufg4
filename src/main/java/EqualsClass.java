87ce57e960f8eae3c8042ebd9fda90e44be8f2d7
Paul <pa721raa@htwg-konstanz.de>
2021-01-07 18:31:47 +0100

    add mystring

--- src/main/java/EqualsClass.java
+++
@@ -1,4 +0,0 @@
-public class EqualsClass {
-    String s = "s";
-
-}
---
+++ src/test/java/MyStringTest.java
@@ -0,0 +1,116 @@
+import org.junit.jupiter.api.*;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyStringTest {
+
+    private MyString myString;
+    private Object anObject;
+    private Boolean result;
+
+    @Test
+    public void testSameTypeDifferentString() {
+        // Set Up
+        this.myString = new MyString("a");
+        this.anObject = new MyString("b");
+        // Execute
+        this.result = myString.equals(anObject);
+        // Verify
+        assertFalse(result);
+        // Teardown
+        this.myString = null;
+        this.anObject = null;
+        this.result = null;
+    }
+
+    @Test
+    public void testSameObject() {
+        //Set Up
+        this.myString = new MyString("a");
+        this.anObject = myString;
+        //Exercise
+        this.result = myString.equals(anObject);
+        //Verify
+        assertTrue(result);
+        //TearDown
+        this.myString = null;
+        this.anObject = null;
+        this.result = null;
+    }
+
+    @Test
+    public void testSameTypeSameString() {
+        // Set Up
+        this.myString = new MyString("a");
+        this.anObject = new MyString("a");
+        // Execute
+        this.result = myString.equals(anObject);
+        // Verify
+        assertTrue(result);
+        // Teardown
+        this.myString = null;
+        this.anObject = null;
+        this.result = null;
+    }
+
+    @Test
+    public void testDifferentTypeSameString() {
+        //Set Up
+        this.myString = new MyString("a");
+        this.anObject = new String("a");
+        //Exercise
+        this.result = myString.equals(anObject);
+        //Verify
+        assertFalse(result);
+        //TearDown
+        this.myString = null;
+        this.anObject = null;
+        this.result = null;
+    }
+
+    @Test
+    public void testDifferentLength() {
+        // Set Up
+        this.myString = new MyString("aa");
+        this.anObject = new MyString("aaa");
+        // Execute
+        result = myString.equals(anObject);
+        // Verify
+        assertFalse(result);
+        // Teardown
+        this.myString = null;
+        this.anObject = null;
+        this.result = null;
+    }
+
+    @Test
+    public void testEmpty() {
+        // Set Up
+        this.myString = new MyString("");
+        this.anObject = new MyString("");
+        // Execute
+        result = myString.equals(anObject);
+        // Verify
+        assertTrue(result);
+        // Teardown
+        this.myString = null;
+        this.anObject = null;
+        this.result = null;
+    }
+
+    @Test
+    public void testSpaceAndEmpty() {
+        // Set Up
+        this.myString = new MyString("");
+        this.anObject = new MyString(" ");
+        // Execute
+        result = myString.equals(anObject);
+        // Verify
+        assertFalse(result);
+        // Teardown
+        this.myString = null;
+        this.anObject = null;
+        this.result = null;
+    }
+}
